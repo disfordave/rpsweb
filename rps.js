3299362c24e0555c6f002f437c08af862aa99695
reikavik <118544840+reikavik@users.noreply.github.com>
2023-10-18 21:49:23 +0900

    Ditch alert to show results, add HTML scoreboard

--- rps.js
+++ rps.js
@@ -3,42 +3,98 @@ let comChoice;
 
 const options = ["rock", "paper", "scissors"];
 
+let attempts = 0;
+let win = 0;
+let draw = 0;
+let lose = 0;
+
+const result = document.getElementById("result-p");
 
 
 /* function getComputerChoice(params) {
     return 
 } */
 
-function RockButton() {
-    userChoice = "rock";
-    PlayRPS();
-}
-function PaperButton() {
-    userChoice = "paper";
-    PlayRPS();
-}
-function ScissorsButton() {
-    userChoice = "scissors";
-    PlayRPS();
+function getLocalStorage() {
+    attempts = Number(localStorage.getItem("attempts"));
+    win = Number(localStorage.getItem("win"));
+    draw = Number(localStorage.getItem("draw"));
+    lose = Number(localStorage.getItem("lose"));
+    updateScoreboard();
 }
 
-function PlayRPS() {
+getLocalStorage();
+
+
+document.getElementById("reset-result-button").addEventListener("click", () => {
+    localStorage.clear();
+    getLocalStorage();
+    result.innerHTML = "Choose one!"
+})
+
+function updateScoreboard() {
+    
+    document.getElementById("attempts").innerHTML = attempts;
+    document.getElementById("win").innerHTML = win;
+    document.getElementById("draw").innerHTML = draw;
+    document.getElementById("lose").innerHTML = lose;
+    
+}
+
+function setLocalStorage() {
+    localStorage.setItem("attempts", attempts);
+    localStorage.setItem("win", win);
+    localStorage.setItem("draw", draw);
+    localStorage.setItem("lose", lose);
+}
+
+const rockBtn = document.getElementById("rock-button");
+const paperBtn = document.getElementById("paper-button");
+const scissorsBtn = document.getElementById("scissors-button");
+
+
+
+rockBtn.addEventListener("click", rockButton);
+paperBtn.addEventListener("click", paperButton);
+scissorsBtn.addEventListener("click", scissorsButton)
+
+function rockButton() {
+    userChoice = "rock";
+    playRPS();
+}
+function paperButton() {
+    userChoice = "paper";
+    playRPS();
+}
+function scissorsButton() {
+    userChoice = "scissors";
+    playRPS();
+}
+
+function playRPS() {
     comChoice = options[Math.floor(Math.random() * options.length)];
 
     /*userChoice = prompt("Rock, paper, scissors?");
     userChoice = userChoice.toLowerCase()*/
+    attempts += 1;
 
     if (comChoice == userChoice) {
         console.log("DRAW.")
-        alert(`Your choice was ${userChoice}, computer's choice was ${comChoice}, DRAW.`)
+        result.innerHTML = `Your choice was ${userChoice}, computer's choice was ${comChoice}, DRAW.`;
+        draw += 1;
     } else if (userChoice == "rock" && comChoice == "paper" || userChoice == "paper" && comChoice =="scissors" || userChoice=="scissors" && comChoice == "rock") {
         console.log("YOU LOSE!")
-        alert(`Your choice was ${userChoice}, computer's choice was ${comChoice}, YOU LOSE!`)
+        result.innerHTML = `Your choice was ${userChoice}, computer's choice was ${comChoice}, YOU LOSE!`;
+        lose += 1;
     } else if (comChoice == "rock" && userChoice == "paper" || comChoice == "paper" && userChoice =="scissors" || comChoice=="scissors" && userChoice == "rock") {
         console.log("YOU WIN!")
-        alert(`Your choice was ${userChoice}, computer's choice was ${comChoice}, YOU WIN!`)
+        result.innerHTML = `Your choice was ${userChoice}, computer's choice was ${comChoice}, YOU WIN!`;
+        win += 1;
     } else {
         console.log("INVALID INPUT.")
-        alert(`INVALID INPUT.`)
+        result.innerHTML = `INVALID INPUT.`;
     }
+
+    updateScoreboard();
+    setLocalStorage();
 }
